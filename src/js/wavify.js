4d2783307235d552222ca5e16297d117a7451673
witflash <witflash@gmail.com>
2018-02-21 15:24:54 +0200

    create waves on header

--- src/js/wavify.js
+++ src/js/wavify.js
@@ -146,24 +146,3 @@
     };
 
 }(jQuery));
-$('.feel-the-wave').each( function(index, el) {
-	console.log(index, el);
-	$(el).wavify({
-	  height: 50,
-	  bones: 5,
-	  amplitude: 30,
-		color: 'rgba(255, 255, 255, 0.05)',
-	  speed: .1
-	});
-  });
-  
-  $('.feel-the-wave-two').each( function(index, el) {
-	console.log(index, el);
-	$(el).wavify({
-	  height: 40,
-	  bones: 4,
-	  amplitude: 30,
-		color: 'rgba(255, 255, 255, 0.05)',
-	  speed: .1
-	});
-  });
