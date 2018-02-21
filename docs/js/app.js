54ff2b2ae74b454d870d417c308918ae62da9df1
witflash <witflash@gmail.com>
2018-02-22 01:00:35 +0200

    create slick slider

--- docs/js/app.js
+++ docs/js/app.js
@@ -146,8 +146,51 @@
     };
 
 }(jQuery));
+$(document).ready(function() {
+  $('.slick').slick({
+		adaptiveHeight: false,
+		dots: false,
+		mobileFirst: true,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		responsive: [
+			{
+				breakpoint: 1024,
+				settings: {
+					slidesToShow: 5,
+					slidesToScroll: 5,
+					infinite: true,
+					dots: true
+				}
+			},
+			{
+				breakpoint: 750,
+				settings: {
+					slidesToShow: 4,
+					slidesToScroll: 4,
+					dots: true					
+				}
+			},
+			{
+				breakpoint: 550,
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 3,
+					dots: true
+				}
+			},
+			{
+				breakpoint: 400,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 2
+				}
+			}
+		]
+  });
+});
+
 $('.feel-the-wave').each( function(index, el) {
-	console.log(index, el);
 	$(el).wavify({
 	  height: 50,
 	  bones: 5,
@@ -158,7 +201,6 @@ $('.feel-the-wave').each( function(index, el) {
   });
   
   $('.feel-the-wave-two').each( function(index, el) {
-	console.log(index, el);
 	$(el).wavify({
 	  height: 40,
 	  bones: 4,
@@ -166,4 +208,4 @@ $('.feel-the-wave').each( function(index, el) {
 		color: 'rgba(255, 255, 255, 0.05)',
 	  speed: .1
 	});
-  });
+	});
--- src/js/_app.js
+++ src/js/_app.js
@@ -1,5 +1,48 @@
+$(document).ready(function() {
+  $('.slick').slick({
+		adaptiveHeight: false,
+		dots: false,
+		mobileFirst: true,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		responsive: [
+			{
+				breakpoint: 1024,
+				settings: {
+					slidesToShow: 5,
+					slidesToScroll: 5,
+					infinite: true,
+					dots: true
+				}
+			},
+			{
+				breakpoint: 750,
+				settings: {
+					slidesToShow: 4,
+					slidesToScroll: 4,
+					dots: true					
+				}
+			},
+			{
+				breakpoint: 550,
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 3,
+					dots: true
+				}
+			},
+			{
+				breakpoint: 400,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 2
+				}
+			}
+		]
+  });
+});
+
 $('.feel-the-wave').each( function(index, el) {
-	console.log(index, el);
 	$(el).wavify({
 	  height: 50,
 	  bones: 5,
@@ -10,7 +53,6 @@ $('.feel-the-wave').each( function(index, el) {
   });
   
   $('.feel-the-wave-two').each( function(index, el) {
-	console.log(index, el);
 	$(el).wavify({
 	  height: 40,
 	  bones: 4,
@@ -18,4 +60,4 @@ $('.feel-the-wave').each( function(index, el) {
 		color: 'rgba(255, 255, 255, 0.05)',
 	  speed: .1
 	});
-  });
+	});
